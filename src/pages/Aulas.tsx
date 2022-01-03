2261b283885df15e0c5e8c3ccce5be53923ee9a0
LucasCruz <lucaas-cruuz@live.com>
2022-01-03 03:06:26 -0300

    excluir funcionando

--- src/pages/Aulas.tsx
+++ src/pages/Aulas.tsx
@@ -1,18 +1,41 @@
-import { useEffect } from "react";
-import { Table } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Button, Modal, Table, Toast, ToastContainer } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { useAulas } from '../hooks/useAulas';
+import { Api } from "../providers";
 
 
 export function Aulas() {
 
     const { aulas, getAll } = useAulas();
+    const [show, setShow] = useState(false);
+    const [showToast, setShowToast] = useState(false);
+    const [idDelete, setIdDelete] = useState(0);
 
     useEffect(() => {
         getAll();
     }, [getAll]);
+
+    function handleDeleteAula(id: number) {
+        setShow(true);
+        setIdDelete(id);
+    }
+
+    function handleClose() {
+        setShow(false);
+    }
+
+    async function deleteAula() {
+        const response = await Api.delete('/Aulas/' + idDelete)
+
+        if (response.status === 204) {
+            setShowToast(true);
+            getAll();
+        }
+        setShow(false);
+    }
 
     return (
         <>
@@ -39,8 +62,8 @@ export function Aulas() {
                                 <td>{item.data}</td>
                                 <td>{item.moduloId}</td>
                                 <td>
-                                    <a href='#'>Editar</a>
-                                    <a href='#'>Excluir</a>
+                                    <Link className="btn btn-warning" to={"/editalivro/" + item.id}>Editar</Link>
+                                    <a className="btn btn-warning" onClick={() => handleDeleteAula(item.id)} >Excluir</a>
                                 </td>
                             </tr>
                         ))}
@@ -49,6 +72,39 @@ export function Aulas() {
                 </Table>
                 <Link to={'/addaula'} className="button">Adicionar Aula</Link>
             </div>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Excluir Aula</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Você tem certeza que deseja excluir esta aula?
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Não
+                    </Button>
+                    <Button variant="primary" onClick={() => { deleteAula() }}>Tenho certeza</Button>
+                </Modal.Footer>
+            </Modal>
+            <ToastContainer className="p-3" position={'top-center'}>
+                <Toast onClose={() => setShowToast(false)} show={showToast} delay={3000} autohide>
+                    <Toast.Header closeButton={false}>
+                        <img
+                            src="holder.js/20x20?text=%20"
+                            className="rounded me-2"
+                            alt=""
+                        />
+                        <strong className="me-auto">Sucesso!</strong>
+                        <small>now</small>
+                    </Toast.Header>
+                    <Toast.Body>Aula excluida com sucesso</Toast.Body>
+                </Toast>
+            </ToastContainer>
             <Footer />
         </>
     );
--- src/pages/Modulos.tsx
+++ src/pages/Modulos.tsx
@@ -1,19 +1,42 @@
-import { useEffect } from 'react';
-import { Table } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { Button, Modal, Table, Toast, ToastContainer } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import { useModulo } from '../hooks/useModulos';
+import { Api } from '../providers';
 import '../styles/modulos.scss'
 
 
 export function Modulos() {
 
     const { modulos, getAll } = useModulo();
+    const [show, setShow] = useState(false);
+    const [showToast, setShowToast] = useState(false);
+    const [idDelete, setIdDelete] = useState(0);
 
     useEffect(() => {
         getAll();
     }, [getAll]);
+
+    function handleDeleteModulo(id: number) {
+        setShow(true);
+        setIdDelete(id);
+    }
+
+    function handleClose() {
+        setShow(false);
+    }
+
+    async function deleteModulo() {
+        const response = await Api.delete('/Modulos/' + idDelete)
+
+        if (response.status === 204) {
+            setShowToast(true);
+            getAll();
+        }
+        setShow(false);
+    }
 
     return (
         <>
@@ -36,8 +59,8 @@ export function Modulos() {
                                 <td>{item.id}</td>
                                 <td>{item.nome}</td>
                                 <td>
-                                    <a href='#'>Editar</a>
-                                    <a href='#'>Excluir</a>
+                                    <Link className="btn btn-warning" to={"/editalivro/" + item.id}>Editar</Link>
+                                    <a className="btn btn-warning" onClick={() => handleDeleteModulo(item.id)} >Excluir</a>
                                 </td>
                             </tr>
                         ))}
@@ -46,6 +69,39 @@ export function Modulos() {
                 </Table>
                 <Link to={'/addmodulo'} className="button" >Adicionar Modulo</Link>
             </div>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Excluir Aula</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Você tem certeza que deseja excluir esta aula?
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Não
+                    </Button>
+                    <Button variant="primary" onClick={() => { deleteModulo() }}>Tenho certeza</Button>
+                </Modal.Footer>
+            </Modal>
+            <ToastContainer className="p-3" position={'top-center'}>
+                <Toast onClose={() => setShowToast(false)} show={showToast} delay={3000} autohide>
+                    <Toast.Header closeButton={false}>
+                        <img
+                            src="holder.js/20x20?text=%20"
+                            className="rounded me-2"
+                            alt=""
+                        />
+                        <strong className="me-auto">Sucesso!</strong>
+                        <small>now</small>
+                    </Toast.Header>
+                    <Toast.Body>Modulo excluido com sucesso</Toast.Body>
+                </Toast>
+            </ToastContainer>
             <Footer />
         </>
     );
